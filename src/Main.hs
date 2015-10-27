a04f7f5c3a1885e01f4f6bc41bd9ae22dab27760
Ian Denhardt <ian@zenhack.net>
2015-10-27 00:38:21 -0400

    Infrastructure for quickcheck tests
    Mostly instances of Arbitrary for some types.

--- src/Main.hs
+++ src/Main.hs
@@ -14,6 +14,8 @@
     You should have received a copy of the GNU General Public License
     along with this program. If not, see <http://www.gnu.org/licenses/>
 -}
+module Main where
+
 import qualified System.Posix.Files as PF
 import qualified System.Posix.Types as PT
 import System.Directory (getDirectoryContents, createDirectoryIfMissing, doesFileExist)
--- tests/TestMain.hs
+++ tests/TestMain.hs
@@ -1,7 +1,85 @@
+{-# LANGUAGE FlexibleInstances #-}
 module TestMain where
 
-import Main hiding (main)
+import Data.UnixTime (UnixTime(..), toEpochTime)
+import qualified System.Posix.Files.ByteString as PFB
+import qualified System.Posix.Types as PT
+import qualified System.Posix.User as PU
+import qualified Main as Main
+import Main ((//))
 import Test.Framework
+import Test.QuickCheck.Arbitrary (Arbitrary, arbitrary)
+import Test.QuickCheck.Gen (oneof)
+
+-- We want to be able to do testing as a regular user, so we'll construct our
+-- examples using a UID/GID that we actually control. @unsafePerformIO@ lets
+-- us have constants, rather than having to thread the IO monad through
+-- everything to get a value that's never going to change.
+import System.IO.Unsafe (unsafePerformIO)
+effectiveUID = unsafePerformIO PU.getEffectiveUserID
+effectiveGID = unsafePerformIO PU.getEffectiveGroupID
+
+-- handy to have an off-the-shelf time we can use.
+epoch1 = toEpochTime (UnixTime 1 0)
+
+data FileType = Directory
+              | RegularFile
+              | Symlink
+              deriving(Eq)
+
+typeMode :: FileType -> PT.FileMode
+typeMode typ = case typ of
+    Directory -> PFB.directoryMode
+    RegularFile -> PFB.regularFileMode
+    Symlink -> PFB.symbolicLinkMode
+
+
+instance Arbitrary FileType where
+    arbitrary = oneof $ map return [Directory, RegularFile, Symlink]
+
+data FileStatus = FileStatus { mode  :: PT.FileMode
+                             , owner :: PT.UserID
+                             , group :: PT.GroupID
+                             , atime :: PT.EpochTime
+                             , ctime :: PT.EpochTime
+                             }
+
+instance Arbitrary FileStatus where
+    arbitrary = do
+        status <- FileStatus <$>     return PFB.ownerModes
+                                 <*> return effectiveUID
+                                 <*> return effectiveGID
+                                 <*> return epoch1
+                                 <*> return epoch1
+        return status
+
+hasMode :: PT.FileMode -> PT.FileMode -> Bool
+hasMode all check = PFB.intersectFileModes all check == check
+
+instance Main.FileStatus FileStatus where
+    isRegularFile s  = hasMode (mode s) PFB.regularFileMode
+    isDirectory s    = hasMode (mode s) PFB.directoryMode
+    isSymbolicLink s = hasMode (mode s) PFB.symbolicLinkMode
+    fileMode         = mode
+    fileOwner        = owner
+    fileGroup        = group
+    accessTime       = atime
+    modificationTime = ctime
+
+sampleFileNames = map (:[]) ['a'..'z']
+
+instance Arbitrary (Main.FileTree FileStatus) where
+    arbitrary = oneof [ Main.Directory <$> mkName <*> mkStatus Directory <*> arbitrary
+                      , Main.RegularFile <$> mkName <*> mkStatus RegularFile
+                      , Main.Symlink <$> mkName <*> mkStatus Symlink
+                      ]
+      where
+        mkStatus ty = do
+            status <- arbitrary
+            return $ status { mode = PFB.unionFileModes (mode status)
+                                                        (typeMode ty) }
+        mkName = oneof $ map return sampleFileNames
+
 
 main :: IO ()
 main = defaultMain []
