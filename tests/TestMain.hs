b73fe916ce8391f8b3089e2702096c1a02f7171e
Ian Denhardt <ian@zenhack.net>
2015-11-06 19:56:30 -0500

    Sloppy: keep tempdir after failed test
    This lets us actually examine the generated file tree. The
    implementation is sloppy; it should be cleaned up at some point.

--- tests/TestMain.hs
+++ tests/TestMain.hs
@@ -10,6 +10,7 @@ import qualified DedupBackup
 import DedupBackup ((//))
 
 import System.Unix.Directory (withTemporaryDirectory)
+import System.Directory (createDirectoryIfMissing, removeDirectoryRecursive)
 
 readThenWriteEq :: Property
 readThenWriteEq = monadicIO $ do
@@ -17,10 +18,21 @@ readThenWriteEq = monadicIO $ do
     -- XXX TODO: we need this type annotation, but this is an awkward spot for
     -- it:
     let _ = tree :: (DedupBackup.FileTree FileStatus)
-    readBack <- run $ withTemporaryDirectory "testsuite.XXXXXX" (\path -> do
+--    readBack <- run $ withTemporaryDirectory "testsuite.XXXXXX" (\path -> do
+ --   readBack <- run $ do
+    ok <- run $ do
+        let path = "/tmp/foo/bar"
+        createDirectoryIfMissing True path
         writeTree (path // "src") tree
-        DedupBackup.lStatTree (path // "src"))
-    assert $ sameTree tree readBack
+        readBack <- DedupBackup.lStatTree (path // "src")
+        let ok = sameTree tree readBack
+        if ok then
+            removeDirectoryRecursive path
+        else
+            return ()
+        return ok
+    assert ok
+--    assert $ sameTree tree readBack
 
 
 main :: IO ()
